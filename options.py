f71eb0517c847dd84ba3ae90c6eabedfc0894920
Ostap Ivanyshyn <ostap.solomon@gmail.com>
2021-04-02 16:49:08 +0300

    fix typo :pencil:

--- options.py
+++ options.py
@@ -1,5 +1,5 @@
 def mainframe_opts(parser):
-  group = parser.add_argument_group('Translation')
+  group = parser.add_argument_group('Mainframe')
   group.add_argument('--private', default=False,
         help='start in private mode without voice input')
   return group
